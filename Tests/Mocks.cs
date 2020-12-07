8527bc4ccf1408c198b88dd32b10b6ab2d6c224f
Joe VanWanzeele <jvanwanzeele@keyfactor.com>
2020-12-07 11:56:18 -0500

    removed unnecessary mock data.

--- Tests/Mocks.cs
+++ Tests/Mocks.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Keyfactor.Platform.Extensions.Agents;
 using Keyfactor.Platform.Extensions.Agents.Delegates;
@@ -18,28 +17,10 @@ namespace Keyfactor.AnyAgent.WindowsUser.Tests
 
         public static AnyJobConfigInfo GetMockConfig()
         {
-            var ajStore = new AnyJobStoreInfo()
-            {
-                Properties = new
-                {
-                    VaultUrl = "https://test.vault",
-                    TenantId = "8b74a908-b153-41dc-bfe5-3ea7b22b9678",
-                    ClientSecret = "testClientSecret",
-                    ApplicationId = Guid.NewGuid().ToString(),
-                    SubscriptionId = Guid.NewGuid().ToString(),
-                    VaultName = "testVaultName",
-                    ResourceGroupName = "testResourceGroupName",
-                    APIObjectId = Guid.NewGuid().ToString(),
-                },
-                Inventory = new List<AnyJobInventoryItem>(),
-                StorePath = "http://test.vault",
-                Storetype = 1,
-            };
             var ajJob = new AnyJobJobInfo { OperationType = Platform.Extensions.Agents.Enums.AnyJobOperationType.Create, Alias = "testJob", JobId = Guid.NewGuid(), JobTypeId = Guid.NewGuid(), };
             var ajServer = new AnyJobServerInfo { Username = "testUsername", Password = "testPassword", UseSSL = true };
             var ajc = new AnyJobConfigInfo()
             {
-                Store = ajStore,
                 Job = ajJob,
                 Server = ajServer
             };
